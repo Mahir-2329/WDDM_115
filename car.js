816d6fe3f709a5d5c5edf9d3b73fb3296215ae9e
Mahir-2329 <mahirpansuriya85@gmail.com>
2023-03-09 14:03:48 -0500

    make change

---
+++ car.js
@@ -0,0 +1,17 @@
+class Car {
+    constructor(_make, _model, _year, _color) {
+        this.make = _make;
+        this.model = _model;
+        this.year = _year;
+        this.color = _color;
+    }}
+    var make = "Honda";
+    var model = "Civic";
+    var year = "2019";
+    var color = "Silver";
+    
+    newFunction();
+
+function newFunction() {
+    changeColor(_color); { this.color = _color; }
+}
---
+++ mscript.js
@@ -0,0 +1,5 @@
+const myCar = new Car("Mini", "Cooper", "2023", "Red");
+console.log("MY CAR\nMAKE: " + myCar.make + "\nMODEL: " + myCar.model + "\nYEAR: " + myCar.year + "\nCOLOR: " + myCar.color);
+myCar.changeColor("Black");
+
+  
